cecdc5d0b2004cbaeb3e95e2fc9b5656a053ed2c
Meenu Pandey <pandeymeenu057@gmail.com>
2026-01-13 21:06:47 +0530

    Delete backend directory

--- backend/src/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-function connectDB() {
-  mongoose.connect(process.env.MONGODB_URL).then(() => console.log('DATABASE CONNECTED')).catch(err => console.log(err))
-}
-
-
-module.exports = connectDB
